7e873e6c2226e649868a20fa846c18fa5f49a808
unknown <ak375456@gmail.com>
2025-08-19 04:25:44 -0700

    AGP upgrade

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.lexur.yumo"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
