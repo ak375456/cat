86924bf4ef6ee40d9cad832db9c615eb16fcc52c
unknown <ak375456@gmail.com>
2025-08-27 09:52:19 -0700

    Pushed update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.lexur.yumo"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.0.5"
+        versionCode = 8
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
